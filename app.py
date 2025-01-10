29e63b3fd0b2f68190686c2a25d1d2640afd3e29
Pablo A. <pa.nonread@gmail.com>
2025-01-10 08:15:30 +0100

    pages from template

--- app.py
+++ app.py
@@ -9,24 +9,39 @@ from flask import render_template
 app = Flask(__name__)
 
 
+# Replace the existing home function with the one below
+"""    
 @app.route("/")
 def home():
     return "Hello, Flask!"
+"""
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+# New functions
+@app.route("/about/")
+def about():
+    return render_template("about.html")
+
+@app.route("/contact/")
+def contact():
+    return render_template("contact.html")
+
 
 
 @app.route("/hello/")
 @app.route("/hello/<name>")
 def hello_there(name = None):
-    fichero="hello_there.html"
     return render_template(
-        fichero,
+        "hello_there.html",
         name=name,
         date=datetime.now()
     )
 
 # serve static file in code
 # usado para api's
-# 
 @app.route("/api/data")
 def get_data():
     return app.send_static_file("data.json")
